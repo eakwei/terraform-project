0d8fe1c82bb5c540db8da6cb085ec170bfa10731
eakwei <62135768+eakwei@users.noreply.github.com>
2023-07-18 18:46:27 +0000

    Delete New_Setups directory

--- New_Setups/main.tf
+++
@@ -1,9 +0,0 @@
-module "new_deployment" {
-  source = "../modules/web_db_service"
-  region = "eu-west-2"
-  aws_vpc-website-vpc = "10.0.0.0/16"
-  tenancy = "default"
-  pub_sub_cidr = ["10.0.1.0/24", "10.0.2.0/24"]
-  priv_app_sub_cidr = ["10.0.3.0/24", "10.0.4.0/24"]
-  instance_type = "t2.micro"
-}
